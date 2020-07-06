7b6f2b911c09351c26d2479c85a0f89c4c886de1
Slibetube <54892254+slibetube@users.noreply.github.com>
2020-07-06 15:34:39 +0200

    Add files via upload

---
+++ Chatmail/Receiver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatmail
+{
+    class Receiver
+    {
+        private int Id;
+        private int ReceiverId;
+        private int MessageId;
+
+        public int id
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+
+        public int receiverId
+        {
+            get { return ReceiverId; }
+            set { ReceiverId = value; }
+        }
+
+        public int messageId
+        {
+            get { return MessageId; }
+            set { MessageId = value; }
+        }
+
+
+        //Constructor
+        public Receiver(int id, int receiverId, int messageId)
+        {
+            this.id = id;
+            this.receiverId = receiverId;
+            this.messageId = messageId;
+        }
+    }
+}
---
+++ Chatmail/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatmail
+{
+    class User
+    {
+        private int Id;
+        private string Name;
+
+        public int id
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+        public string name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+        public User(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
